81608401def777d7fdf08fabfc1b44f2df1cc8de
Dean Yang <dean@wintec.com.tw>
2021-04-29 13:21:39 +0800

    [新增] 興櫃股價查詢

--- stock.php
+++ stock.php
@@ -10,31 +10,54 @@
 <body>
 <?php
 $stock = $_GET['stock'];
-$url = 'https://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=tse_'.$stock.'.tw&json=1';
-$userAgent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36';
-$http_header[] = "Accept-Language: zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7";
-echo '<a target="_blank" href='.$url.'>'.$url.'</a><br>';
-/* initial the curl object */
-$curl = @curl_init();
-@curl_setopt($curl , CURLOPT_URL , $url);
-@curl_setopt($curl , CURLOPT_GET , true );
-@curl_setopt($curl , CURLOPT_USERAGENT, $userAgent);
-@curl_setopt($curl , CURLOPT_FORBID_REUSE , true );// persistent, keepalive
-@curl_setopt($curl , CURLOPT_RETURNTRANSFER , true );
-//@curl_setopt($curl , CURLOPT_SSL_VERIFYPEER , false );// ssl verify
-@curl_setopt($curl , CURLOPT_FAILONERROR, true);
 
-@curl_setopt($curl , CURLOPT_HTTPHEADER  , $http_header);
-//@curl_setopt($curl , CURLOPT_POSTFIELDS , $http_content );
-$contents = @curl_exec( $curl );
-if(@curl_errno($curl))
-{	
-	$error_msg = @curl_error($curl);
+function getData($stock, $type='tse')
+{
+	$url = 'https://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch='.$type.'_'.$stock.'.tw&json=1&delay=0';
+	$userAgent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36';
+	$http_header[] = "Accept-Language: zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7";
+	
+	/* initial the curl object */
+	$curl = @curl_init();
+	@curl_setopt($curl , CURLOPT_URL , $url);
+	@curl_setopt($curl , CURLOPT_GET , true );
+	@curl_setopt($curl , CURLOPT_USERAGENT, $userAgent);
+	@curl_setopt($curl , CURLOPT_FORBID_REUSE , true );// persistent, keepalive
+	@curl_setopt($curl , CURLOPT_RETURNTRANSFER , true );
+	//@curl_setopt($curl , CURLOPT_SSL_VERIFYPEER , false );// ssl verify
+	@curl_setopt($curl , CURLOPT_FAILONERROR, true);
+
+	@curl_setopt($curl , CURLOPT_HTTPHEADER  , $http_header);
+	//@curl_setopt($curl , CURLOPT_POSTFIELDS , $http_content );
+	$contents = @curl_exec( $curl );
+	if(@curl_errno($curl))
+	{	
+		$error_msg = @curl_error($curl);
+	}
+	if(isset($error_msg))
+	{
+		echo "\$error_msg=$error_msg<br>";
+		return NULL;
+	}
+		
+	$json = json_decode($contents, true);
+	if($json && isset($json['msgArray']) && count($json['msgArray']))
+	{
+		echo '<a target="_blank" href='.$url.'>'.$url.'</a><br>';
+		return $json['msgArray'][0];
+	}
+	else
+		return NULL;
 }
-if(isset($error_msg))
-	echo "\$error_msg=$error_msg<br>";
-$json = json_decode($contents, true);
-$data = $json['msgArray'][0];
+$data = [];
+if(!empty($stock))
+{
+	$data = getData($stock);
+	if(empty($data))
+		$data = getData($stock,'otc');
+}
+
+
 $strStockName = htmlspecialchars($data['n'], ENT_COMPAT);
 $strStockNum = $data['c'];
 $strStockValPrev = $data['y'];
